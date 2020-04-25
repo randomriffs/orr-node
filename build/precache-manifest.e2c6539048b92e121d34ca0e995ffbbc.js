375e205c0f89d169c72ebb1973ab1c107e3671e2
Pratheesh Eswaran <pratheesh@Pratheeshs-MacBook-Pro.local>
2020-04-25 13:37:24 +0530

    added react to build folder

---
+++ build/precache-manifest.e2c6539048b92e121d34ca0e995ffbbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4ab3d884ba45b5421a56447059a31a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a6f9b7584591fb5ef7",
+    "url": "/static/css/main.76e2ca9b.chunk.css"
+  },
+  {
+    "revision": "99bccfaf765245fe6886",
+    "url": "/static/js/2.b98112bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b98112bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a6f9b7584591fb5ef7",
+    "url": "/static/js/main.22caf2e7.chunk.js"
+  },
+  {
+    "revision": "8255a5a361d5df12c3c4",
+    "url": "/static/js/runtime-main.41ff3ee5.js"
+  }
+]);
