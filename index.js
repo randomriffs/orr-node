6dc992961ea1eb7ae7cffbf634c79a1e0c82ba29
Pratheesh Eswaran <pratheesh@Pratheeshs-MacBook-Pro.local>
2020-04-25 11:19:09 +0530

    initial commit for node server

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = new express();
+app.use(cors());
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const User = require('./models/userSchema')
+const Blogs = require('./models/blogSchema');
+const Tasks = require('./models/taskSchema')
+
+
+// User.create({
+//     name: 'test name',
+//     email: 'email',.b
+//     password: 'password'
+// });
+
+app.post('/api/login', (req, res) => {
+    let userStatus = {}
+    console.log('req.bodys',req.body)
+    User.findOne({
+        email: req.body.email,
+        password:req.body.password
+    }).then((user)=>{
+        if(user){
+            res.json({status:true,message:'user exists'})
+        }else {
+            res.json({status:false,message:'user not exists'})
+        }
+        
+    });
+    
+})
+getAllBlogPost = (req,res) =>{
+    Blogs.find({
+    }, (err, result)=> {
+        if (err) throw err;
+        if (result) {
+            res.json(result.reverse())
+        } else {
+            res.send(JSON.stringify({
+                error : 'Error'
+            }))
+        }
+    })
+}
+app.get('/api/blogs',(req,res)=>{
+    getAllBlogPost(req,res)
+})
+
+getAllTask=(req,res)=>{
+    Tasks.find({},(err,result)=>{
+        if (err) throw err;
+        if(result){
+            res.json(result)
+        }else {
+            res.send(JSON.stringify({error:'error'}))
+        }
+    })
+}
+app.get('/api/tasks',(req,res)=>{
+getAllTask(req,res)
+})
+
+app.post('/api/add-post', (req, res) => {
+    console.log('req.bodys',req.body)
+    Blogs.create({
+    title: req.body.title,
+    content: req.body.content,
+}).then((data)=>{
+    if(data){
+        getAllBlogPost(req,res)
+    }
+}); 
+})
+
+app.post('/api/add-task', (req, res) => {
+    console.log('req.bodys',req.body)
+    Tasks.create({
+    task: req.body.task,
+    isDone: req.body.isDone,
+}).then((data)=>{
+    if(data){
+        getAllTask(req,res)
+    }
+}); 
+})
+
+app.get('/test', (req,res)=>{
+    res.json({status:'sucess'})
+})
+
+app.listen(1234,()=>{
+    console.log('server is running up')
+})
---
+++ models/userSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require("../database");
+const schema = {
+    name: { type: mongoose.SchemaTypes.String, required: true },
+    email: { type: mongoose.SchemaTypes.String, required: true },
+    password: { 
+        type: mongoose.SchemaTypes.String, 
+        required: true, 
+        select: false
+    }
+};
+const collectionName = "user"; // Name of the collection of documents
+const userSchema = mongoose.Schema(schema);
+const User = mongoose.model(collectionName, userSchema);
+module.exports = User;
