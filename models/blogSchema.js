dd2e823f63475a6408078aa31c924032e9811870
Pratheesh Eswaran <pratheesh@Pratheeshs-MacBook-Pro.local>
2020-04-25 16:33:35 +0530

    deleted and update added

--- models/blogSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("../database");
-const schema = {
-    title: { type: mongoose.SchemaTypes.String, required: true },
-    content: { type: mongoose.SchemaTypes.String, required: true },
-};
-const collectionName = "blogs"; // Name of the collection of documents
-const blogSchema = mongoose.Schema(schema);
-const Blogs = mongoose.model(collectionName, blogSchema);
-module.exports = Blogs;
