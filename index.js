a21027aaad2619482fa210e993b67fa39946e8d6
Pratheesh Eswaran <pratheesh@Pratheeshs-MacBook-Pro.local>
2020-04-26 23:29:00 +0530

    added preacher app to randomriff

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const app = new express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3002;
+var fs = require('fs');
+var admin = require("firebase-admin");
+var serviceAccount = require("./config/words-movies-firebase-adminsdk-udl0t-722049ea9f.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://words-movies.firebaseio.com"
+});
+let db = admin.firestore();
+let filmList = ['dunkirk', 'forrest-gump', 'moonlight', 'no-country-for-old-men', 'pulp-fiction', 'spotlight', 'blood-diamond'
+    , 'django-unchained', 'fight-club', 'inglourious-basterds', 'cuckoo-nest', 'reservoir-dogs', 'monte-cristo', 'godfather'
+    , 'grand-budapest', 'godfather-2']
 
 const User = require('./models/userSchema')
 const Blogs = require('./models/blogSchema');
@@ -128,6 +139,61 @@ app.post('/api/delete-task', (req, res) => {
             }
         })
 })
+
+app.get('/api/random', (req, res) => {
+    let totalFilmList = filmList.length
+    let randomFilm = Math.floor(Math.random() * totalFilmList)
+    console.log(filmList[randomFilm])
+    var docRef = db.collection("words").doc(filmList[randomFilm]);
+    docRef.get().then(function (doc) {
+        if (doc.exists) {
+            let sayings = doc.data().data;
+            let totalSayings = sayings.length - 1;
+            let randomSaying = Math.floor(Math.random() * totalSayings) + 1
+            console.log('total sayings', sayings.length)
+            console.log('random saying id', randomSaying)
+            console.log(sayings[randomSaying])
+            res.json({ status: true, sayings: sayings[randomSaying] })
+
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+            res.json({ status: false })
+        }
+    }).catch(function (error) {
+        console.log("Error getting document:", error);
+    });
+
+})
+
+
+// extracting words 
+
+
+// fs.readFile('./subtitlefiles/The.Godfather.Part.II.1974.BRRip.XviD.AC3.D-Z0N3 (English).srt', 'utf8', function (err, data) {
+//     if (err) throw err;
+//     let arrowSplitData = data.split('-->')
+//     let dataRemovedData = arrowSplitData.filter(data => data !== 'data');
+//     let dialogue = []
+//     dataRemovedData.forEach((data) => {
+//         let onlyDialogue = data.replace(/[^A-Za-z, .?!']+/g, '');
+//         let removedComma = onlyDialogue.slice(2, onlyDialogue.length - 2);
+//         let removedI = ''
+//         if (removedComma[0] === 'i') {
+//             removedI = removedComma.slice(2, removedComma.length - 2)
+//         }
+//         dialogue.push(removedComma.trim(' '));
+//     })
+//     let removeFirstAndLast = dialogue.slice(10, dialogue.length - 10)
+//     console.log('removeFirstAndLast', removeFirstAndLast)
+
+//     db.collection("words").doc('godfather-2').set({
+//         data: removeFirstAndLast
+//     }, { merge: true }).then(() => {
+//         console.log('data saved')
+//     }).catch((err) => console.log(err))
+// });
+
 
 app.listen(port, () => {
     console.log('server is running up')
