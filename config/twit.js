c64ca8c661ad0a3c71cf627207e4998069676c15
Pratheesh Eswaran <pratheesh@Pratheeshs-MacBook-Pro.local>
2020-04-27 11:52:00 +0530

    adding twit config file

---
+++ config/twit.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'V7JBVZZuazn4c0ZWCFb8eRNsG',
+    consumer_secret: 'RAagmE8ETTrhHep11h6Pdn8VICbjNCpnlBhNksjF3bKhzvvMMw',
+    access_token:'1214166740776546307-FoT7ggTmhZOdIY30NGi5tgPFWJKC2X',
+    access_token_secret: 'EF71CAAguOaF2h0fYGjYQVhJSXzCmWZxDxmmFsU9CETII'
+}
