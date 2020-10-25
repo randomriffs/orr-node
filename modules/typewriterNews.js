0781b142363e451213e40a13a4cc3e8e644a43ee
Pratheesh Eswaran <pratheesh@Pratheeshs-MacBook-Pro.local>
2020-10-25 20:53:26 +0530

    updated new api to everything

--- modules/typewriterNews.js
+++ modules/typewriterNews.js
@@ -5,54 +5,61 @@ module.exports.tweet = () => {
     const config = require('../config/typewriterNews');
     var TinyURL = require('tinyurl');
     const Twitter = new Twit(config);
-    let SourceUrl = "https://newsapi.org/v2/sources?apiKey="+process.env.NEWAPI_KEY;
-    let newsUrl = "https://newsapi.org/v2/top-headlines?apiKey="+process.env.NEWAPI_KEY;
+    let SourceUrl = "https://newsapi.org/v2/sources?apiKey=" + process.env.NEWAPI_KEY;
+    let newsUrl = "https://newsapi.org/v2/everything?apiKey=" + process.env.NEWAPI_KEY;
     let tweet = {
         status: '*******'
     }
-console.log('calling source url')
-    axios.get(SourceUrl)
-        .then(function (response) {
-            console.log('source url call completed')
-            let responseData = response.data
-            let sourcesLength = responseData.sources && responseData.sources.length-1;
-            let randomSource = Math.floor(Math.random() * (sourcesLength - 0) + 0);
-            let randomCategory = responseData.sources[randomSource].category
-            let apiUrl = newsUrl+"&q="+randomCategory
-            console.log('calling article api ',apiUrl)
-            axios.get(apiUrl).then(function(newsResponse){
-                console.log('article api sucessfull')
-                let newsData = newsResponse.data;
-                let articleLength = newsData.articles.length;
-                let randomrArticle = Math.floor(Math.random() * (articleLength - 0) + 0);
-                let articleTitle = newsData.articles[randomrArticle].title;
-                let articleUrl = newsData.articles[randomrArticle].url;
-                // const tinyUrlData = { 'url': articleUrl}
+    console.log('calling source url')
+    fetchNewsApi = () => {
+        axios.get(SourceUrl)
+            .then(function (response) {
+                console.log('source url call completed')
+                let responseData = response.data
+                let sourcesLength = responseData.sources && responseData.sources.length - 1;
+                let randomSource = Math.floor(Math.random() * (sourcesLength - 0) + 0);
+                let randomCategory = responseData.sources[randomSource].category
+                let apiUrl = newsUrl + "&q=" + randomCategory
+                console.log('calling article api ', apiUrl)
+                axios.get(apiUrl).then(function (newsResponse) {
+                    console.log('article api sucessfull')
+                    let newsData = newsResponse.data;
+                    let articleLength = newsData.articles.length;
+                    let randomrArticle = Math.floor(Math.random() * (articleLength - 0) + 0);
+                    let articleTitle = newsData.articles[randomrArticle].title;
+                    let articleUrl = newsData.articles[randomrArticle].url;
+                    // const tinyUrlData = { 'url': articleUrl}
 
-                TinyURL.shorten(articleUrl).then(function (shortenUrl) {
-                    // console.log('tinyurl',res)
-                    tweet.status = `${articleTitle} ${shortenUrl}`
-                    console.log('tweet',tweet)
-                    Twitter.post('statuses/update', tweet, tweeted)
-                    function tweeted(err, firstData, response) {
-                       if (err) {
-                           console.log("Something went wrong!", err);
-                       }
-                       else {
-                           console.log('News tweeted')
-                       }
-                   }
-                }, function (err) {
-                    console.log(err)
+                    TinyURL.shorten(articleUrl).then(function (shortenUrl) {
+                        // console.log('tinyurl',res)
+                        tweet.status = `${articleTitle} ${shortenUrl}`
+                        console.log('tweet', tweet)
+                        Twitter.post('statuses/update', tweet, tweeted)
+                        function tweeted(err, firstData, response) {
+                            if (err) {
+                                console.log("Something went wrong!", err);
+                            }
+                            else {
+                                console.log('News tweeted')
+                            }
+                        }
+                    }, function (err) {
+                        console.log(err)
+                        fetchNewsApi()
+                    })
+
+                }).catch(err => {
+                    console.log('error in article call')
+                    fetchNewsApi()
                 })
 
-            }).catch(err=>{
-                console.log('error in article call')
             })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+                fetchNewsApi()
+            })
+    }
+    fetchNewsApi();
 
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
 }
